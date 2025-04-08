fded188765af3cd57027ae0aed1b4819c2f962ce
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-07 23:01:31 -0300

    Conclusão do desafio 2 e 3
    concluir a o desafio 2 e 3 criando duas variáveis, uma variável nome e outra variável idade e exibi em uma mensagem de alerta

---
+++ declarar_variavel_dsf-2/app.js
@@ -0,0 +1,5 @@
+let nome = 'lua';           //Atribuição correta da variável
+alert ('Olá, ' + nome);      //Concatenando a string 
+
+let idade = 25;             //Atribuição correta da variável
+alert ('Sua idade é ' + idade + ' anos')            //// Concatenando a idade com o texto
