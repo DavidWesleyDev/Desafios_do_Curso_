e312cfbe0d67c30adb711f83c65217cacdd1fdb9
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-07 23:19:10 -0300

    Concluir o desafio 4 e 5
    Definimos as variáveis e mostramos por meio de um alert

---
+++ Numero_de_vendas_dsf-4_e_5/app.js
@@ -0,0 +1,5 @@
+let numeroDeVendas = 50;         // Atribui o valor 50 à variável 'numeroDeVendas', representando o total de vendas realizadas
+alert ('O numero de vendas dessa semanda foi de : ' + numeroDeVendas + ' vendas') ;     // Exibe uma mensagem mostrando o total de vendas concatenado com o texto
+
+let soldoDisponivel = 1000;             // Atribui o valor 1000 à variável 'soldoDisponivel', representando o saldo disponível
+alert ('Após as 50 vendas o saldo disponivel é de : ' + soldoDisponivel + ' Reais')          // Exibe uma mensagem mostrando o saldo disponível concatenado com o texto
