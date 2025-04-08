d736e8f96bfbdbe7ef09721353d65c69cb1e7e18
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-08 17:28:01 -0300

    Finalização dos exercícios
    finalizei os 10 exercícios que foi passado para ternamento

---
+++ desafio_6_7_8/app.js
@@ -0,0 +1,16 @@
+//Desafio 6
+alert('Erro! Preencha todos os campos')
+
+//Desafio 7
+let mensagemDeErro = 'Erro! Preencha todos os campos';          //declarei a variavel mensagemDeErro 
+alert (mensagemDeErro);                                         //chamei o valor da variável no alert 
+
+//Desafio 8
+let nome = prompt ('Qual é o seu nome ?');                      //declarei que a variavel nome seria escolhida quando foi chamado a manesagem do prompt
+alert ('Olá ! ' + nome);                                        //exibe a mensagem do alert seguido da variável 
+
+//desafio 9 e 10
+let idade = prompt ('Qual a sua idade??')
+if (idade >= 18);
+
+alert ('Pode tirar a habilitação')
